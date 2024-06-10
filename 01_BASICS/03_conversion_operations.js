6a9766a08b0e29869215b1482fe05adb3e624bc5
sakshishri86 <144041872+sakshishri86@users.noreply.github.com>
2024-06-10 15:48:25 +0000

    03 basic

---
+++ 01_BASICS/03_conversion_operations.js
@@ -0,0 +1,29 @@
+let score ="33abc"
+console.log(typeof score)
+
+
+let valueInnumber = Number(score)   //converted string to number by defining it number
+console.log(typeof valueInnumber)
+console.log(valueInnumber)   // gives NaN as output i.e. not an number bcz value of score converted
+                            // to number but given input is not number.
+// "33"=> 33     
+// "33abc" => NaN
+//   true => 1;  false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) 
+
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber );
+
+//************operations****************** 
+
+
