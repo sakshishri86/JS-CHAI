fa3f445f80eff0d657076ac4ceb7bb82045146da
sakshishri86 <144041872+sakshishri86@users.noreply.github.com>
2024-06-11 06:25:48 +0000

    datatypes summary

---
+++ 01_BASICS/05_datatypes_summary.js
@@ -0,0 +1,32 @@
+// primitive data type
+// 7 types : string,number,boolean,null, undefined,symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+// symbols
+// const id = Symbol('123')
+// const anotherId = symbol('123') 
+// // both will give different output 
+
+// BigInt => put "n"  at the end
+const bigNumber = 345261736358139876545n
+
+
+
+// non primitive or refrence data types
+// memory - directly value allocate
+ // array, objects ,functions
+ // js is dynamicaly typed 
+
+const heroes = ["shaktiman","naagraj","doga"];  //ARRAY
+let myObj ={
+    name: "hitesh",
+    age:  22,                 //OBJECT
+}
+
+const myFunction = function(){
+    console.log("hello world");      //FUNCTION
+}
+
+ // REVISE AND CHECK SOME ERROR
